21dd89317e116aa6ab4a99089ea9e73032544637
Lorenzo <cingano.lorenzo2002@gmail.com>
2022-03-18 14:26:37 +0100

    Fully implemented CorsoCorsistaDAO and added CorsoCorsistaDAOAdapter

--- project/src/main/java/com/betacom/architecture/dao/CorsoCorsistaDAO.java
+++ project/src/main/java/com/betacom/architecture/dao/CorsoCorsistaDAO.java
@@ -1,14 +1,17 @@
 package com.betacom.architecture.dao;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.sql.rowset.CachedRowSet;
 import javax.sql.rowset.RowSetProvider;
 
 import com.betacom.businesscomponent.model.CorsoCorsista;
 
-public class CorsoCorsistaDAO extends GenericDAO<CorsoCorsista> implements DAOConstants {
+public class CorsoCorsistaDAO extends CorsoCorsistaDAOAdapter implements DAOConstants {
 	private CachedRowSet rowSet;
 	
 	private CorsoCorsistaDAO() throws DAOException {
@@ -34,29 +37,47 @@ public class CorsoCorsistaDAO extends GenericDAO<CorsoCorsista> implements DAOCo
 			throw new DAOException(exc);
 		}
 	}
-
-	@Override
-	public void update(Connection conn, CorsoCorsista entity) throws DAOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void delete(Connection conn, CorsoCorsista entity) throws DAOException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public CorsoCorsista getById(Connection conn, long id) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+	
+	public CorsoCorsista getByCourse(Connection conn, CorsoCorsista entity) throws DAOException {
+		CorsoCorsista corsoCorsista = new CorsoCorsista();
+		try {
+			PreparedStatement ps = conn.prepareStatement(SELECT_CORSO_CORSISTA_BYID);
+			ps.setLong(1, entity.getCodCorso());
+			
+			ResultSet rs = ps.executeQuery();
+			if(rs.next()) {
+				corsoCorsista.setCodCorso(rs.getLong(1));
+				corsoCorsista.setCodCorsista(rs.getLong(2));
+			}
+		} catch (SQLException exc) {
+			throw new DAOException(exc);
+		}
+		return corsoCorsista;
 	}
 
 	@Override
 	public CorsoCorsista[] getAll(Connection conn) throws DAOException {
-		// TODO Auto-generated method stub
-		return null;
+		CorsoCorsista[] corsoCorsisti = null; 
+		
+		try {
+			Statement statement = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			
+			ResultSet rs = statement.executeQuery(SELECT_CORSO_CORSISTA);
+			rs.last();
+			corsoCorsisti = new CorsoCorsista[rs.getRow()];
+			rs.beforeFirst();
+			for(int i = 0; rs.next(); i++) {
+				CorsoCorsista corsoCorsista = new CorsoCorsista();
+				corsoCorsista.setCodCorso(rs.getLong(1));
+				corsoCorsista.setCodCorsista(rs.getLong(2));
+				
+				corsoCorsisti[i] = corsoCorsista;
+			}
+		} catch (SQLException exc) {
+			throw new DAOException(exc);
+		}
+		
+		return corsoCorsisti;
 	}
 	
 }
