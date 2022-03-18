c7389c6ecb858a9783f6a5981e911ede8750d226
Ale <Betacom@DESKTOP-1FDM76K>
2022-03-18 13:07:08 +0100

    Tabelle create

--- project/SQL/create_obj.sql
+++ project/SQL/create_obj.sql
@@ -1,3 +1,25 @@
+create table corsista(
+	cod_corsista int,
+	nome_corsista varchar2(30) not null,
+	cognome_corsista varchar2(30) not null,
+	prec_form number(1,0) not null,
+	constraint p_cod_corsista primary key(cod_corsista));
+
+create sequence corsista_seq;
+
+create table amministratore(
+	cod_admin int,
+	nome_admin varchar2(30) not null,
+	cognome_admin varchar2(30) not null,
+	constraint p_cod_admin primary key(cod_admin));
+	
+create table docente(
+	cod_docente int,
+	nome_docente varchar2(30) not null,
+	cognome_docente varchar2(30) not null,
+	cv_docente varchar2(200) not null,
+	constraint p_cod_docente primary key(cod_docente));
+
 create table corso(
 	cod_corso int, 
 	nome_corso varchar2(30) not null,
@@ -6,13 +28,14 @@ create table corso(
 	costo_corso number(8,2) not null,
 	commenti varchar2(200) not null,
 	aula_corso varchar2(30) not null,
-	constraint p_cod_corso primary key(cod_corso)
-)
-
-create table corsista(
+	docente int,
+	constraint p_cod_corso primary key(cod_corso),
+	constraint f_docente foreign key(docente) references docente(cod_docente) on delete cascade);
+	
+create table corso_corsista(
+	cod_corso int,
 	cod_corsista int,
-	nome_corsista varchar2(30) not null,
-	cognome_corsista varchar2(30) not null,
-	prec_form number(1,0) not null,
-	constraint p_cod_corsista primary key(cod_corsista)
-)
+	constraint p_cc primary key(cod_corso, cod_corsista),
+	constraint f_cod_corso foreign key(cod_corso) references corso(cod_corso) on delete cascade,
+	constraint f_cod_corsista foreign key(cod_corsista) references corsista(cod_corsista) on delete cascade);
+
---
+++ project/SQL/insert_obj.sql
@@ -0,0 +1,13 @@
+insert into amministratore values(1, 'Pippo', 'Rossi');
+
+insert into docente values(541, 'Marisa', 'Biondi', 'Sono studiato');
+insert into docente values(6811, 'Max', 'Verdi', 'Sarebbi studiato');
+insert into docente values(63103, 'Luca', 'Arancio', 'Fossi studiato');
+insert into docente values(800, 'Giuseppe', 'Rossi', 'Avessi stato studiato');
+
+insert into corso values(1, 'Influencer', '12-SET-2000', '02-GIU-2001', 2000.00, 'Fatte cose', 'A1', 541);
+insert into corso values(2, 'Media Management', '10-SET-2002', '05-GIU-2003', 1000.00, 'Trovate tutto sul mio profilo instagram', 'B2', 6811);
+insert into corso values(3, 'Corso per haters', '15-SET-2005', '07-GIU-2006', 1500.00, 'Parlato male', 'C3', 63103);
+insert into corso values(4, 'Corricorso', '09-SET-2000', '05-GIU-2001', 3000.00, 'Imparato a imparare', 'D4', 800);
+insert into corso values(5, 'Corso covid', '04-SET-2002', '06-GIU-2001', 1800.00, 'Partecipato al covid party', 'C19', 541);
+insert into corso values(6, 'Corso per Saiyan', '13-SET-2003', '08-GIU-2004', 3200.00, 'Potenziato aura', 'C18', 63103);
