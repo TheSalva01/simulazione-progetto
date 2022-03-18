a0efeb509578c2fd28a711611c51e7e61a8536dd
Lorenzo <cingano.lorenzo2002@gmail.com>
2022-03-18 14:11:15 +0100

    Updated DAOConstants

--- project/src/main/java/com/betacom/architecture/dao/DAOConstants.java
+++ project/src/main/java/com/betacom/architecture/dao/DAOConstants.java
@@ -3,5 +3,6 @@ package com.betacom.architecture.dao;
 public interface DAOConstants {
 
 	String SELECT_CORSO_CORSISTA = "select * from corso_corsista";
+	String SELECT_CORSO_CORSISTA_BYID = "select * from corso_corsista where cod_corso = ?";
 	
 }
