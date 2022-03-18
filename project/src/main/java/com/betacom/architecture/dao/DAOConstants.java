528bd7acaa81e2e54818739125d23bfac9da6ccb
Ale <Betacom@DESKTOP-1FDM76K>
2022-03-18 14:16:29 +0100

    Updated DAOConstants

--- project/src/main/java/com/betacom/architecture/dao/DAOConstants.java
+++ project/src/main/java/com/betacom/architecture/dao/DAOConstants.java
@@ -5,4 +5,5 @@ public interface DAOConstants {
 	String SELECT_CORSO_CORSISTA = "select * from corso_corsista";
 	String SELECT_CORSO_CORSISTA_BYID = "select * from corso_corsista where cod_corso = ?";
 	
+	String SELECT_CORSISTA_SEQ = "select corsista_seq.nextval from dual";
 }
