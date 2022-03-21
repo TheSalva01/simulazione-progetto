6b175575f3aab8c847b8e13e402e619a485f63f6
Ale <Betacom@DESKTOP-1FDM76K>
2022-03-21 12:31:09 +0100

    AdminFacade aggiornata

---
+++ project/src/main/java/test/com/betacom/businesscomponent/facade/AdminFacadeTest.java
@@ -0,0 +1,106 @@
+package test.com.betacom.businesscomponent.facade;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.betacom.architecture.dao.DAOException;
+import com.betacom.architecture.dbaccess.DBAccess;
+import com.betacom.businesscomponent.CorsistaBC;
+import com.betacom.businesscomponent.CorsoBC;
+import com.betacom.businesscomponent.facade.AdminFacade;
+import com.betacom.businesscomponent.model.Corsista;
+import com.betacom.businesscomponent.model.Corso;
+import com.betacom.businesscomponent.model.CorsoCorsista;
+
+class AdminFacadeTest {
+	Connection conn;
+	Corsista corsista;
+	static Corso corso;
+	CorsoCorsista cc;
+	
+	@BeforeAll
+	static void setBeforeAll() throws Exception {
+		corso = new Corso();
+		corso.setCodCorso(7);
+		corso.setAulaCorso("AB12");
+		corso.setCommentiCorso("commento corso");
+		corso.setCostoCorso(1500.00);
+		corso.setDocente(800);
+		corso.setNomeCorso("corso di prova");
+		GregorianCalendar gc = new GregorianCalendar(2, 9, 2021);
+		corso.setDataInizioCorso(gc.getTime());
+		corso.setDataFineCorso(new Date());
+		
+		CorsoBC corsoBC = new CorsoBC();
+		corsoBC.create(corso);
+		System.out.println("Corso di prova creato");
+	}
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		conn = DBAccess.getConnection();
+		corsista = new Corsista();
+		corsista.setNomeCorsista("Mario");
+		corsista.setCognomeCorsista("Giallo");
+		corsista.setPrecForm(1);
+		
+		cc = new CorsoCorsista();
+		cc.setCodCorso(7);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+		try {
+			CorsistaBC cBC = new CorsistaBC();
+			cBC.delete(corsista);
+			System.out.println("Tabelle ripulite");
+		} catch(DAOException exc) {
+			exc.printStackTrace();
+			fail("Errore nella pulizia dei record");
+		}
+	}
+
+	@Test
+	void testCreateCorsista() {
+		try {
+			AdminFacade.getInstance().createCorsista(corsista, corso);
+			System.out.println("Corsista creato");
+		} catch(DAOException | ClassNotFoundException | IOException exc) {
+			exc.printStackTrace();
+			fail("Errore nel CreateCorsista");
+		}
+	}
+	
+	@Test
+	void testDeleteCorso(){
+		try {
+			AdminFacade.getInstance().deleteCorso(corso);
+			System.out.println("Corso eliminato");
+		} catch(DAOException | ClassNotFoundException | IOException exc) {
+			exc.printStackTrace();
+			fail("Errore nel DeleteCorso");
+		}
+	}
+	
+	@AfterAll
+	static void tearAfterAll() throws Exception {
+		try {
+			CorsoBC corsoBC = new CorsoBC();
+			corsoBC.delete(corso);
+			System.out.println("Tabelle ripulite");
+		} catch(DAOException exc) {
+			exc.printStackTrace();
+			fail("Errore nella pulizia dei record");
+		}
+	}
+}
